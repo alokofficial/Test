545d20e45e6e63b223d0e08628f5d283548a9dea
Alok Kumar <aloknh31@gmail.com>
2025-12-03 12:16:43 +0000

    start working on header part

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,9 +1,10 @@
 import Login from './Components/Login'
+import Header from './Components/Header'
 import './app.css'
 function App() {
   return (
     <>
-      <div className='bg-green-400 text-red-600 text-4xl'>EpiTCUA</div>
+      <Header />
       <Login />
     </>
   )
---
+++ React/src/Components/Header.jsx
@@ -0,0 +1,14 @@
+const Header =()=>{
+    return(
+        <>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+export default Header
