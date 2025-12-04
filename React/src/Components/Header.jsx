de67f83eb8fff09bf8a7c10a794aab394659f5fa
Alok Kumar <aloknh31@gmail.com>
2025-12-04 11:17:14 +0000

    worked on header login logout button

--- React/src/Components/Header.jsx
+++ React/src/Components/Header.jsx
@@ -1,11 +1,22 @@
+import { PiSignIn, PiSignOut } from "react-icons/pi";
+import { BsCart } from "react-icons/bs";
+import { useState } from "react";
 const Header =()=>{
+    const [login, setLogin] = useState(true)
+    const handleLogin =()=>{
+        setLogin(!login)
+    }
     return(
         <>
-            <div>
-                <ul>
-                    <li>Home</li>
+            <div className="bg-emerald-200 shadow-gray-900 m-0 flex justify-between content-center ">
+                <ul className='flex m-2 gap-4 list-none'>
+                    <li >Home</li>
                     <li>Products</li>
-                    <li>Contact Us</li>
+                    <li>ContactUs</li>
+                </ul>
+                <ul className="flex m-2 gap-4 list-none">
+                    <li><BsCart /></li>
+                    <li onClick={handleLogin} >{login?<PiSignIn />:<PiSignOut />}</li>
                 </ul>
             </div>
         </>
